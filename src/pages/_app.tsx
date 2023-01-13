2700e8d5469b97ab0aaff98d05594e6a33a897d6
Matheus Barreto <matheuspbarreto@hotmail.com.br>
2023-01-12 22:48:42 -0300

    wip: fix in _document

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,13 @@
+import { useEffect } from 'react'
 import '@styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() =>{
+    document.querySelector('#jss-server-side')?.remove();
+  }, []);
+
   return (
     <>
       <Head>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,7 +8,7 @@ export default class MyDocument extends Document {
     originalRenderPage = ctx.renderPage;
 
     ctx.renderPage = () => originalRenderPage({
-      enhanceApp: (App) => (props)=> sheets.collect(<App {...props} />)
+      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />)
     });
 
     const initialProps = await Document.getInitialProps(ctx);
@@ -26,7 +26,10 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <title>Documento de teste</title>
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+          />
         </Head>
 
         <body>
